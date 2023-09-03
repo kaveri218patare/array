121447f7d3d6e7a9bcdff18410cde0d15e8721dc
Kaveri Patare <143255148+kaveri218patare@users.noreply.github.com>
2023-09-03 16:52:18 +0530

    Add files via upload

---
+++ maximum.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+   int a[100],i,n,max;
+   printf("enter limit");
+   scanf("%d",&n);
+   printf("enter n numbers");
+   for(i=0;i<n;i++)
+   {
+    scanf("%d",&a[i]);
+   }
+    max=a[0];
+   for(i=0;i<n;i++)
+   {
+    if(a[i]>max)
+    max=a[i];
+   }
+   printf("maximum no=%d",max);
+}
+
